edcd8eae3e46230a33c7cf7ab0a8d3da0c62331a
Your Name <you@example.com>
2023-02-28 17:55:52 +0530

    minor changes

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
 // import { Fonts } from './Src/Components/CustomText';
 // import MainNav from './Src/Navigations/Route';
 // import { Provider } from 'react-redux';
 // import { mystore } from './Src/Redux/Store/Store';
 import AppNavContainer from './src/Navigations';
-import { Text, View } from 'react-native';
-import RNBootSplash from "react-native-bootsplash";
+import {Text, View} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 import Theme from './src/Components/common/Theme';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
+import PushNotification from 'react-native-push-notification';
+
 
 const App = ({navigation}) => {
-  useEffect(()=>{
+  useEffect(() => {
     RNBootSplash.hide();
-  },[])
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // console.log('A new FCM message arrived!', JSON.stringify(remoteMessage))
+      Alert.alert(remoteMessage?.notification?.title, remoteMessage?.notification?.body);
+     
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <>
       <StatusBar
-      // backgroundColor={Theme.white} 
+      // backgroundColor={Theme.white}
       />
       <AppNavContainer />
     </>
--- src/ApiService/ApiService.js
+++ src/ApiService/ApiService.js
@@ -1,7 +1,7 @@
 
 // LIVE URL
-const API_SERVICES_URL = 'https://2101-2405-201-9017-5815-206d-3e3c-1d7a-c151.in.ngrok.io/';
-// const API_SERVICES_URL = 'http://35.78.190.246/';
+// const API_SERVICES_URL = 'https://d694-2405-201-9017-5815-75e4-18cb-15b7-df4f.in.ngrok.io/';
+const API_SERVICES_URL = 'http://35.78.190.246/';
 // const API_SERVICES_URL = 'http://35.78.190.246:8081/';
 
 
