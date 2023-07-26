337bfb46e54a703c5b1c5f011fbd746cc0dcfa1e
PROSENJIT SARKAR <bestprosenjit@gmail.com>
2023-07-26 14:30:50 +0530

    Change app name

--- ios/hphinternalTests/hphinternalTests.m
+++ ios/hphinternalTests/hphinternalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface hphmanagementTests : XCTestCase
+@interface hphinternalTests : XCTestCase
 
 @end
 
-@implementation hphmanagementTests
+@implementation hphinternalTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
